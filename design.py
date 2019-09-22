1e6f36d1a004da3776875bfc3d828bd7ec615334
sylvain chassang <sylvain.chassang@gmail.com>
2019-09-22 19:32:58 -0400

    interim commit

--- design.py
+++ design.py
@@ -1,5 +1,4 @@
 from hashlib import md5
-from functools import partial
 import random
 import abc
 
@@ -10,6 +9,7 @@ import numpy as np
 from assignment import draw_iid_assignment, draw_shuffled_assignment, \
     get_assignments_by_positions
 from balance import BalanceObjective
+from utils import QuantileTarget
 
 
 class RCTBase:
@@ -71,16 +71,12 @@ class RCT(RCTBase):
         return self._draw_shuffled_assignment()
 
 
-class KRerandomizedRCT(RCTBase):
+class BalancedRCTBase(RCTBase):
     def __init__(self, objective: BalanceObjective,
                  file_path, weights, k=None, seed=0):
         super().__init__(file_path, weights, seed)
         self._balance = objective.balance_func
         self._k = k
-
-    def balance(self, assignment):
-        return float(self._balance(
-            self.df, get_assignments_by_positions(assignment)).values)
 
     @property
     def k(self):
@@ -88,19 +84,59 @@ class KRerandomizedRCT(RCTBase):
             self._k = self.sample_size
         return self._k
 
+    def balance(self, assignment):
+        return float(self._balance(
+            self.df, get_assignments_by_positions(assignment)).values)
+
+    def assignment_generator(self, draw_fun):
+        return (draw_fun(self.weights, self.sample_size) for _ in range(
+            self.k))
+
+
+class KRerandomizedRCT(BalancedRCTBase):
     @property
     def assignment_from_iid(self):
         np.random.seed(self.seed)
-        assignments = (draw_iid_assignment(self.weights, self.sample_size)
-                       for _ in range(self.sample_size))
+        assignments = self.assignment_generator(draw_iid_assignment)
         return self._get_best_assignment(assignments)
 
     @property
     def assignment_from_shuffled(self):
         random.seed(self.seed)
-        assignments = (draw_shuffled_assignment(
-            self.weights, self.sample_size) for _ in range(self.sample_size))
+        assignments = self.assignment_generator(draw_shuffled_assignment)
         return self._get_best_assignment(assignments)
 
     def _get_best_assignment(self, assignments):
         return self.as_frame(max(assignments, key=self.balance))
+
+
+class QuantileTargetingRCT(BalancedRCTBase):
+    def __init__(self, objective: BalanceObjective, file_path, weights,
+                 quantile_target=None, seed=0, num_monte_carlo=1000):
+        super().__init__(file_path, weights, seed)
+        self._balance = objective.balance_func
+        self.quantile_target = quantile_target
+        self._k = num_monte_carlo
+
+    def balance(self, assignment):
+        return float(self._balance(
+            self.df, get_assignments_by_positions(assignment)).values)
+
+    @property
+    def assignment_from_iid(self):
+        np.random.seed(self.seed)
+        assignments = self.assignment_generator(draw_iid_assignment)
+        return self.get_target_assignments(assignments)
+
+    @property
+    def assignment_from_shuffled(self):
+        random.seed(self.seed)
+        assignments = self.assignment_generator(draw_shuffled_assignment)
+        return self.get_target_assignments(assignments)
+
+    def get_target_assignments(self, assignments):
+        qtargets = QuantileTarget(
+            self.quantile_target, self.balance, assignments)
+        qtargets.compute_best()
+        _, selected_assignment = random.choice(qtargets.quantiles)
+        return selected_assignment
